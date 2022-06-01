653fde9abfce989397874f6962a7d38c41a1fe42
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-06-01 22:11:07 +0100

    Changing of array parsing order

--- index.js
+++ index.js
@@ -359,7 +359,7 @@ app.get("/custom", (req, res) => {
 app.get("/jfcalculator", (req, res) => {
     let otherRoutePromise = Routes.find({routeType: {$ne:"Standard"}}).sort({start: 1, destination: 1}).exec();
     let standardRoutePromise = Routes.find({routeType: "Standard"}).exec();
-    Promise.all([otherRoutePromise, standardRoutePromise]).then((data) => {
+    Promise.all([standardRoutePromise, otherRoutePromise]).then((data) => {
         res.render("jf.ejs", { routes: data[0], otherRoutes: data[1]});
     });
 });
