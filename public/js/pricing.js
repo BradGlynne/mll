8db293de28972152fe7961238a571809c7785d4f
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-06-02 17:37:48 +0100

    modified table to datatable

--- public/js/pricing.js
+++ public/js/pricing.js
@@ -84,10 +84,70 @@ var table = $('#jf-routes').DataTable();
  });
 
 document.getElementById("standard").click();
-document.getElementById("intraDrone").click(); 
+document.getElementById("intraDrone").click();
 
 
 });
+
+$('#per-jump-overrides thead tr')
+    .clone(true)
+    .addClass('filters')
+    .appendTo('#per-jump-overrides thead');
+
+var table = $('#per-jump-overrides').DataTable({
+    orderCellsTop: true,
+    fixedHeader: true,
+    paging: false,
+    order: [[9, 'desc']],
+    initComplete: function () {
+        var api = this.api();
+
+        // For each column
+        api
+            .columns([0,1,3,4,5,6,7])
+            .eq(0)
+            .each(function (colIdx) {
+                // Set the header cell to contain the input element
+                var cell = $('.filters th').eq(
+                    $(api.column(colIdx).header()).index()
+                );
+                var title = $(cell).text();
+                $(cell).html('<input type="text" placeholder="' + title + '" />');
+
+                // On every keypress in this input
+                $(
+                    'input',
+                    $('.filters th').eq($(api.column(colIdx).header()).index())
+                )
+                    .off('keyup change')
+                    .on('keyup change', function (e) {
+                        e.stopPropagation();
+
+                        // Get the search value
+                        $(this).attr('title', $(this).val());
+                        var regexr = '({search})'; //$(this).parents('th').find('select').val();
+
+                        var cursorPosition = this.selectionStart;
+                        // Search the column for that value
+                        api
+                            .column(colIdx)
+                            .search(
+                                this.value != ''
+                                    ? regexr.replace('{search}', '(((' + this.value + ')))')
+                                    : '',
+                                this.value != '',
+                                this.value == ''
+                            )
+                            .draw();
+
+                        $(this)
+                            .focus()[0]
+                            .setSelectionRange(cursorPosition, cursorPosition);
+                    });
+            });
+    },
+});
+
 
 //function intraRoutes(checkbox)
 //{
