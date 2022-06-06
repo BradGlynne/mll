d254fd247feb4b1de81decf242d38f5f7ccd42c1
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-06-06 17:12:27 +0100

    test

--- public/js/darktoggle.js
+++ public/js/darktoggle.js
@@ -1,5 +1,11 @@
 const themeSwitch = document.querySelector('.darkToggle');
 console.log(themeSwitch);
+if(themeSwitch) {
+  console.log("Is True");
+}
+else {
+  console.log ("Is not true");
+}
 themeSwitch.addEventListener('change', () => {
   document.body.classList.toggle('dark-theme');
 });
