ea75ed4d928d73ec4378068d660db142a42133a5
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-06-06 17:04:39 +0100

    test

--- public/js/darktoggle.js
+++ public/js/darktoggle.js
@@ -1,33 +1,5 @@
-// Select the button
-const btn = document.querySelector(".darkToggle");
-// Select the theme preference from localStorage
-const currentTheme = localStorage.getItem("theme");
+const themeSwitch = document.querySelector('.darkToggle');
 
-// If the current theme in localStorage is "dark"...
-if (currentTheme == "dark") {
-  // ...then use the .dark-theme class
-  document.body.classList.add("dark-theme");
-}
-
-// Listen for a click on the button
-btn.addEventListener("click", function() {
-  // Toggle the .dark-theme class on each click
-  document.body.classList.toggle("dark-theme");
-  console.log("Button clicked");
-
-  // Let's say the theme is equal to light
-  let theme = "light";
-  // If the body contains the .dark-theme class...
-  if (document.body.classList.contains("dark-theme")) {
-    // ...then let's make the theme dark
-    theme = "dark";
-  }
-  // Then save the choice in localStorage
-  localStorage.setItem("theme", theme);
-});
-
-
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.documentElement.setAttribute("data-theme", "dark");
+themeSwitch.addEventListener('change', () => {
+  document.body.classList.toggle('dark-theme');
 });
