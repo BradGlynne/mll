78c5902866779b61bdd84933369dae53591e3188
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-06-06 17:11:01 +0100

    debug logging

--- public/js/darktoggle.js
+++ public/js/darktoggle.js
@@ -1,5 +1,5 @@
 const themeSwitch = document.querySelector('.darkToggle');
-
+console.log(themeSwitch);
 themeSwitch.addEventListener('change', () => {
   document.body.classList.toggle('dark-theme');
 });
