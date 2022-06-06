480ad94e0e0b863e4b6dcd49135c5b955778418c
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-06-06 16:32:49 +0100

    test

--- public/js/darktoggle.js
+++ public/js/darktoggle.js
@@ -1,4 +1,4 @@
-		(function () { 
+		(function () {
 			if (localStorage) {
 				var theme = localStorage.getItem('stylemanager.theme');
 				if (!theme && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -9,3 +9,13 @@
 				}
 			}
 		})();
+
+
+    // Select the button
+    const btn = document.querySelector('.darkToggle');
+
+    // Listen for a click on the button
+    btn.addEventListener('click', function() {
+      // Then toggle (add/remove) the .dark-theme class to the body
+      document.body.classList.toggle('dark-theme');
+    })
