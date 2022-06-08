93c878fcc811749a25fcf434b910b316290f3d66
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-06-08 18:24:57 +0100

    removing geolocation search

--- public/js/language.js
+++ public/js/language.js
@@ -34,51 +34,51 @@ $(function () {
       }
     } else {
       // no cookie set, so detect language based on location.
-      if ("geolocation" in navigator) {
-        $('[lang="zh"]').hide();
-        $.cookie('lang', 'en', { expires: 7 });
-        langButtonListen();
-        // geolocation is available
-        navigator.geolocation.getCurrentPosition(function (position) {
-          // accepted geolocation so figure out which country
-          var lat = position.coords.latitude,
-              lng = position.coords.longitude;
-          $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&sensor=true', null, function (response) {
-            var country = response.results[response.results.length-1].formatted_address;
-            if (country ===  'Taiwan' || country === 'China') {
-              $('[lang="en"]').hide();
-              $.cookie('lang', 'zh', { expires: 7 });
-              langButtonListen();
-            } else {
-              $('[lang="zh"]').hide();
-              $.cookie('lang', 'en', { expires: 7 });
-              langButtonListen();
-            }
-          }).fail(function (err) {
-            console.log('error: '+err);
-            $('[lang="zh"]').hide();
-            $.cookie('lang', 'en', { expires: 7 });
-            langButtonListen();
-          });
-        },
-        function (error) {
-          if (error.code == error.PERMISSION_DENIED) {
-            // denied geolocation
-            $('[lang="zh"]').hide();
-            $.cookie('lang', 'en', { expires: 7 });
-            langButtonListen();
-          } else {
-            console.log('Unknown error. Defaulting to English!');
-            $('[lang="zh"]').hide();
-            $.cookie('lang', 'en', { expires: 7 });
-            langButtonListen();
-          }
-        });
-      } else {
+    //   if ("geolocation" in navigator) {
+    //     $('[lang="zh"]').hide();
+    //     $.cookie('lang', 'en', { expires: 7 });
+    //     langButtonListen();
+    //     // geolocation is available
+    //     navigator.geolocation.getCurrentPosition(function (position) {
+    //       // accepted geolocation so figure out which country
+    //       var lat = position.coords.latitude,
+    //           lng = position.coords.longitude;
+    //       $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&sensor=true', null, function (response) {
+    //         var country = response.results[response.results.length-1].formatted_address;
+    //         if (country ===  'Taiwan' || country === 'China') {
+    //           $('[lang="en"]').hide();
+    //           $.cookie('lang', 'zh', { expires: 7 });
+    //           langButtonListen();
+    //         } else {
+    //           $('[lang="zh"]').hide();
+    //           $.cookie('lang', 'en', { expires: 7 });
+    //           langButtonListen();
+    //         }
+    //       }).fail(function (err) {
+    //         console.log('error: '+err);
+    //         $('[lang="zh"]').hide();
+    //         $.cookie('lang', 'en', { expires: 7 });
+    //         langButtonListen();
+    //       });
+    //     },
+    //     function (error) {
+    //       if (error.code == error.PERMISSION_DENIED) {
+    //         // denied geolocation
+    //         $('[lang="zh"]').hide();
+    //         $.cookie('lang', 'en', { expires: 7 });
+    //         langButtonListen();
+    //       } else {
+    //         console.log('Unknown error. Defaulting to English!');
+    //         $('[lang="zh"]').hide();
+    //         $.cookie('lang', 'en', { expires: 7 });
+    //         langButtonListen();
+    //       }
+    //     });
+    //   } else {
         // geolocation IS NOT available
         $('[lang="zh"]').hide();
         $.cookie('lang', 'en', { expires: 7 });
         langButtonListen();
-      }
+      //}
     }
   });
