d3ff48d7aa490a965dab5aa1ad4b0a42ec91b485
emporer213 <dominic.bedwin@ntlworld.com>
2023-04-21 20:24:36 +0100

    aaaaa

--- index.js
+++ index.js
@@ -1500,7 +1500,7 @@ async function processContracts(user) {
     for (contract of userContracts) {
 
         let foundContract = await Contracts.find({contractID: contract.contract_id}).exec()
-        if (foundContract != null) {
+        if (foundContract) {
             if (isNaN(foundContract.start)) {
                 console.log(`Found Contract Start is: ${foundContract.start}    Skipping`)
                 continue;
